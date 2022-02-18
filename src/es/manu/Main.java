49d062aed41d7aeea8f0b5b3434e166e30cdcf51
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-02-18 13:29:39 +0100

    piramide v2

--- src/es/manu/Main.java
+++ src/es/manu/Main.java
@@ -3,9 +3,17 @@ package es.manu;
 public class Main {
 
     public static void main(String[] args) {
-        int count = 0;
-        for (int i = 0; i < 10; i++) {
-
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 10; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
     }
 }
